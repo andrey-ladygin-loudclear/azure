3879ac8a01a89b0acfc0d29905e8bc9bc9b88647
andrey <andrey@gmail.com>
2017-05-17 20:39:39 +0300

    init

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -32,6 +32,10 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
+        if ($exception instanceof Exception) {
+            die('Handler Exception');
+        }
+
         parent::report($exception);
     }
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,6 +4,12 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    private $tables = [
+        'lessons',
+        'tags',
+        'lesson_tag',
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -11,6 +17,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(PostsTableSeeder::class);
+        $this->cleanDB();
+
+        $this->call(PostsTableSeeder::class);
+        $this->call(TagsTableSeeder::class);
+        $this->call(LessonTagTableSeeder::class);
+
+        //php artisan migrate:refresh --seed
+    }
+
+    private function cleanDB()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+        foreach ($this->tables as $table)
+        {
+            DB::table($table)->truncate();
+        }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+
     }
 }
